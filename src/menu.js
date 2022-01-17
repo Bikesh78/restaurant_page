a6d5fdde0f8b5ed16bb19951d67795e2cbf118b2
bikeshrai78 <bikeshrai78@gmail.com>
2022-01-17 21:12:11 +0545

    Make menu.js workable

--- src/menu.js
+++ src/menu.js
@@ -7,27 +7,19 @@ function loadMenu(){
     const price = document.createElement('p');
     const ingredients = document.createElement('p');
     const headerContainer = document.createElement('div');
-    let counter = 0;
+    // let counter = 0;
 
     function Dish(menuId, dishName, price, ingredients){
         this.dishName = dishName;
         this.price = price;
         this.ingredients = ingredients;
-        counter++;
+        // counter++;
     }
     const avocadoToast = new Dish('avocado-toast','Avocado Toast','Rs. 100', 'Avocado and black pepper with egg');
     const strawberryCake = new Dish('strawberryCake','Strawberry Shortcake', 'Rs. 100', 'Strawberry smothered on cake')
     const biscuitGravy = new Dish('biscuit-gravy','Biscuit Gravy', 'Rs. 125','BISCUIT SMOTHERED IN HOUSE MADE SAUSAGE GRAVY OR VEGETARIAN MUSHROOM GRAVY');
     const theDahlia = new Dish('dahlia','The Dahlia','Rs. 250','SAUSAGE PATTY, APPLE BUTTER, FRIED EGG, COVERED IN MAPLE SYRUP');
-    console.log('test to see if counter increases each time Dish is instantiated',counter);
-
-    function createMenuCard(dish){
-        dishName.textContent = dish.dishName;
-        price.textContent = dish.price;
-        ingredients.textContent = dish.ingredients;
-    }
-
-    createMenuCard(avocadoToast);
+    // console.log('test to see if counter increases each time Dish is instantiated',counter);
 
     menuContainer.classList.add('menu-container');
     menuCard.classList.add('menu-card');
@@ -37,13 +29,31 @@ function loadMenu(){
     ingredients.classList.add('ingredients');
     headerContainer.classList.add('header-container');
 
+    function createMenuCard(dish){
+        dishName.textContent = dish.dishName;
+        price.textContent = dish.price;
+        ingredients.textContent = dish.ingredients;
+    }
+
+    
+    // use this array to loop for 'counter' times to call createMenuCard function
+    let dishArray = [avocadoToast,strawberryCake,biscuitGravy,theDahlia]; 
+
+    for(let i = 0; i< dishArray.length; i++){
+        createMenuCard(dishArray[i]);
+        
+        menuCard.appendChild(textContainer);
+        textContainer.appendChild(headerContainer);
+        headerContainer.appendChild(dishName);
+        headerContainer.appendChild(price);
+        textContainer.appendChild(ingredients);
+        menuContainer.appendChild(menuCard.cloneNode(true));
+    }
+
+   
+
     content.appendChild(menuContainer);
-    menuContainer.appendChild(menuCard);
-    menuCard.appendChild(textContainer);
-    textContainer.appendChild(headerContainer);
-    headerContainer.appendChild(dishName);
-    headerContainer.appendChild(price);
-    textContainer.appendChild(ingredients);
+    
 }
 
 export default loadMenu;
